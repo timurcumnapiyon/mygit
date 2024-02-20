84a2662939f6703b53183f9d5fe28f16cd068aff
Timur <timurgcyener@gmail.com>
2024-02-20 17:58:44 +0300

    Добовлена возможность запроса возраста у пользователя

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print("Привет, мир!")
     name = input("Как вас зовуть? ")
     print("Приятно познакомиться " + name + "!")
+    age = input("Сколько вам лет? ")
+    print("Вам уже " + age + "лет!")
 
 if __name__ == "__main__":
     main()
